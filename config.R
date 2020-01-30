467d1ba0ccc2ba939f2f1ef21fb0f0ef6c6e3c95
Ross Searle <ross.searle@csiro.au>
2020-01-30 11:51:33 +1000

    Delete config.R

--- config.R
+++
@@ -1,9 +0,0 @@
-
-#########  Configuration file for the App
-
-dbServer = 'mssql-prod12.it.csiro.au'
-dbDatabase = 'TSV_LNDB'
-dbUser <- 'ShinyWeb'
-dbPwd <- 'h4kcg$$Jt2Y'
-
-defaultStartDate <- '2019-12-01'
